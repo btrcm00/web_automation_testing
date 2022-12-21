27071728e50f62bbe2599dbddff13ceb111de2b6
btcm31 <congminhabcd@gmail.com>
2022-12-21 20:42:34 +0700

    resolve merge conflict

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,5 @@
 from modules.module_Search import TestSearchSuite
 from modules.module_Comment import TestCommentSuite
-from modules.sendMassage import TestSendMassage
-from modules.changePassword import TestChangePassword
+from modules.module_sendMassage import TestSendMassage
+from modules.module_changePassword import TestChangePassword
 from modules.module_ViewAReview import TestVisitareview
